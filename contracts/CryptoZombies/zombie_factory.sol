77e41ba72aa115a458cd413e55ab096dbd7ade30
jfm5344 <jfm6279@gmail.com>
2021-12-18 15:29:21 -0600

    add public array of Zombie structs to store zombie army

--- contracts/CryptoZombies/zombie_factory.sol
+++ contracts/CryptoZombies/zombie_factory.sol
@@ -2,12 +2,15 @@ pragma solidity >=0.5.0 <0.6.0; // solidity version
 
 // create contract:
 contract ZombieFactory {
+    uint256 dnaDigits = 16; // zombie DNA is determined by 16-digit number
+    uint256 dnaModulus = 10**dnaDigits; // to ensure zombie DNA is only 16 chars
 
-    uint dnaDigits = 16; // zombie DNA is determined by 16-digit number
-    uint dnaModulus = 10 ** dnaDigits; // to ensure zombie DNA is only 16 chars
-
+    // create Zombie struct with name and dna properties:
     struct Zombie {
         string name;
-        uint dna;
+        uint256 dna;
     }
+
+    // create public array of Zombie structs & store our zombie army in our app:
+    Zombie[] public zombies; // dynamic can keep growing; can show to other apps
 }
