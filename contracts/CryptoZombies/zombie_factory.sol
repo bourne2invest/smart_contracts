2d8ffe74e5eb170e05152bd73348b25c170c0f7a
jfm5344 <jfm6279@gmail.com>
2021-12-18 14:46:49 -0600

    create state var to ensure dna is only 16 chars

--- contracts/CryptoZombies/zombie_factory.sol
+++ contracts/CryptoZombies/zombie_factory.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.5.0 <0.6.0; // solidity version
 // create contract:
 contract ZombieFactory {
 
-    uint dnaDigits = 16;
+    uint dnaDigits = 16; // zombie DNA is determined by 16-digit number
+    uint dnaModulus = 10 ** dnaDigits; // to ensure zombie DNA is only 16 chars
 
 }
