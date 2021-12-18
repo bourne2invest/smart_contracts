491c240e67410df847ecc4521b3206b4fcfbabf4
jfm5344 <jfm6279@gmail.com>
2021-12-18 15:32:49 -0600

    fix syntax errors

--- contracts/CryptoZombies/example_contract.sol
+++ contracts/CryptoZombies/example_contract.sol
@@ -1,40 +1,37 @@
 pragma solidity >=0.5.0;
 
-contract HelloWorld {
+contract HelloWorld {}
 
-}
-
-contract Example{
-
-    uint myUnsignedInteger = 100; // create uint == 100
+contract Example {
+    uint256 myUnsignedInteger = 100; // create uint == 100
     // this will be stored permanently in the blockchain
 
-    uint x = % **2; // equal to 5^2 == 25 (math in solidity is straightforward)
+    uint256 x = 5**2; // equal to 5^2 == 25 (math in solidity is straightforward)
 
     // create string state var greeting
-    string greeting = "Hello world!"
+    string greeting = "Hello world!";
 
     // create Person struct with age and name properties:
     struct Person {
-        uint age;
+        uint256 age;
         string name;
     }
 
     //// add example arrays
     // array with fixed length of 2 elements:
-    uint[2] fixedArray;
+    uint256[2] fixedArray;
     // another fixed Array, can contain 5 strings:
     string[5] stringArray;
     // a dynamic array with no fixed size and can keep growing:
-    uint[] dynamicArray;
+    uint256[] dynamicArray;
 
     // create an array of structs, named people:
-    Person[] people; // dynamic array, can keep adding to it
+    // Person[] people; // dynamic array, can keep adding to it
+
     // useful for storing structured data in our contract (like a database)
 
     // can declare array as public:
     Person[] public people; // solidity automatically creates getter method
     // other contracts can now read from, but not write to this array
     // this is a useful pattern for storing public data in our contract
-
 }
