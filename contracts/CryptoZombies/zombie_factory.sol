8440e77e06de5cf689c0d2b63fcaa4c7e9e3169c
jfm5344 <jfm6279@gmail.com>
2021-12-19 11:55:31 -0600

    create new zombie (pass fn args to struct and push to array)

--- contracts/CryptoZombies/zombie_factory.sol
+++ contracts/CryptoZombies/zombie_factory.sol
@@ -17,5 +17,8 @@ contract ZombieFactory {
     // create public fn named createZombie
     // accept 2 params: _name::str and _dna::uint
     // pass the 1st param by value using memory keyword
-    function createZombie(string memory _name, uint256 _dna) public {}
+    function createZombie(string memory _name, uint256 _dna) public {
+        // use args to create new Zombie, adding it to zombies array
+        zombies.push(Zombie(_name, _dna));
+    }
 }
