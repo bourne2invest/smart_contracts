6d8782759ae041d853ff480133b4ae3f7225be9d
jfm5344 <jfm6279@gmail.com>
2021-12-19 13:56:29 -0600

    add examples of events

--- contracts/CryptoZombies/example_contract.sol
+++ contracts/CryptoZombies/example_contract.sol
@@ -104,4 +104,25 @@ contract Example {
     uint8 c = a * b;
     // we have to typecast:
     uint8 c = a * uint8(b);
+
+    // events allow our contract to communicate that something
+    // happened on the blockchain to our app front-end, which
+    // may be listening for certain events and take action
+    // when they happen, for e.g:
+    // decalare event
+    event IntegersAdded(uint x, uint y, uint result);
+
+    function add(uint _x, uint _y) public returns (uint) {
+        uint result = _x + _y;
+        // fire an event to let the app know the function was called:
+        emit IntegersAdded(_x, _y, result);
+        return result;
+    }
+
+    // our app front-end could then listen for the event.
+    // a javascript implementation could look like:
+    OurContract.IntegersAdded(function(error, result) {
+        // do something with the result
+    })
+
 }
