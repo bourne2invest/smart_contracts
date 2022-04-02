99dc855175f2123e1895500fc2c52f56fd4f47b6
jfm5344 <jfm6279@gmail.com>
2022-04-02 16:03:20 -0500

    add example inheritance

--- CryptoZombies/contracts/example_contract.sol
+++ CryptoZombies/contracts/example_contract.sol
@@ -178,4 +178,26 @@ contract Example {
         //if True, proceed with the function:
         return "Hi!";
     }
+
+
+    // inheritance.
+    // rather than make one really long contract, it can make sense to split
+    // code logic across multiple contracts to keep code organized, for e.g:
+    contract Doge{
+        function catchphrase() public returns (string memory) {
+            return "So Wow CryptoDoge";
+        }
+    }
+
+    contract BabyDoge is Doge {
+        function anotherCatchphrase() public returns (string memory) {
+            return "Such Moon BabyDoge";
+        }
+    }
+
+    // BabyDoge inherits from Doge, i.e:
+    // if we were to compile and deploy BabyDoge, it would have access to both
+    // catchphrase() and anotherCatchphrase(), as well as any
+    //other public functions defined in Doge.
+
 }
