87973d78d6d6a94107437018c6e46d9bf1b977df
jfm5344 <jfm6279@gmail.com>
2022-04-02 16:16:09 -0500

    better code organization

--- CryptoZombies/contracts/zombie_factory.sol
+++ CryptoZombies/contracts/zombie_factory.sol
@@ -65,5 +65,3 @@ contract ZombieFactory {
         _createZombie(_name, randDna);
     }
 }
-
-contract ZombieFeeding is ZombieFactory {}
---
+++ CryptoZombies/contracts/zombie_feeding.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+//import ZombieFactory contract:
+import "./zombie_factory.sol";
+
+contract ZombieFeeding is ZombieFactory {}
