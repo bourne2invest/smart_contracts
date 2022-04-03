78cf85e3a65ade41ca7257bf9e47db3451c3adaa
jfm5344 <jfm6279@gmail.com>
2022-04-02 20:53:46 -0500

    start feed function, only let zombie owner feed and make Zombie instance

--- CryptoZombies/contracts/zombie_feeding.sol
+++ CryptoZombies/contracts/zombie_feeding.sol
@@ -1,6 +1,14 @@
-pragma solidity >=0.5.0 <0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0; //>=0.5.0 <0.6.0;
 
 //import ZombieFactory contract:
 import "./zombie_factory.sol";
 
-contract ZombieFeeding is ZombieFactory {}
+contract ZombieFeeding is ZombieFactory {
+    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
+        //only let zombie owner feed:
+        require(msg.sender == zombieToOwner[_zombieId]);
+        // declare local Zombie (storage pointer)
+        Zombie storage myZombie = zombies[_zombieId];
+    }
+}
