7a41e89b35905ae45f58eb0b9bb4be0a4d4da53a
jfm5344 <jfm6279@gmail.com>
2022-04-04 21:47:05 -0500

    only owner can modify CK contract address

--- CryptoZombies/contracts/zombie_feeding.sol
+++ CryptoZombies/contracts/zombie_feeding.sol
@@ -28,7 +28,7 @@ contract ZombieFeeding is ZombieFactory {
     KittyInterface kittyContract;
 
     //set crypto kitty contract address
-    function setKittyContractAddress(address _address) external {
+    function setKittyContractAddress(address _address) external onlyOwner {
         kittyContract = KittyInterface(_address);
     }
 
